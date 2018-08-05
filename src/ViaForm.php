8799178fda23d00bb85fba09112d3054dff7904e
Baptiste Langlade <baptouuuu@gmail.com>
2018-08-05 17:51:13 +0200

    add form authenticator

---
+++ src/ViaForm.php
@@ -0,0 +1,32 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\HttpAuthentication;
+
+use Innmind\HttpAuthentication\{
+    ViaForm\Resolver,
+    Exception\NotSupported,
+};
+use Innmind\Http\Message\{
+    ServerRequest,
+    Method,
+};
+
+final class ViaForm implements Authenticator
+{
+    private $resolve;
+
+    public function __construct(Resolver $resolve)
+    {
+        $this->resolve = $resolve;
+    }
+
+    public function __invoke(ServerRequest $request): Identity
+    {
+        if ((string) $request->method() !== Method::POST) {
+            throw new NotSupported;
+        }
+
+        return ($this->resolve)($request->form());
+    }
+}
---
+++ tests/ViaForm/NullResolverTest.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types = 1);
+
+namespace Tests\Innmind\HttpAuthentication\ViaForm;
+
+use Innmind\HttpAuthentication\{
+    ViaForm\NullResolver,
+    ViaForm\Resolver,
+    Exception\AuthenticatorNotImplemented,
+};
+use Innmind\Http\Message\Form;
+use PHPUnit\Framework\TestCase;
+
+class NullResolverTest extends TestCase
+{
+    public function testInterface()
+    {
+        $this->assertInstanceOf(Resolver::class, new NullResolver);
+    }
+
+    public function testInvokation()
+    {
+        $this->expectException(AuthenticatorNotImplemented::class);
+
+        (new NullResolver)($this->createMock(Form::class));
+    }
+}
