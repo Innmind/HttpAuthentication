1ca26624c1a6a380000eb6b1e1e7e7b73c93e2fc
Baptiste Langlade <baptouuuu@gmail.com>
2018-08-11 11:37:12 +0200

    add authenticator to validate authorization header type

---
+++ src/ValidateAuthorizationHeader.php
@@ -0,0 +1,35 @@
+<?php
+declare(strict_types = 1);
+
+namespace Innmind\HttpAuthentication;
+
+use Innmind\HttpAuthentication\Exception\MalformedAuthorizationHeader;
+use Innmind\Http\{
+    Message\ServerRequest,
+    Header\Authorization,
+};
+
+final class ValidateAuthorizationHeader implements Authenticator
+{
+    private $authenticate;
+
+    public function __construct(Authenticator $authenticate)
+    {
+        $this->authenticate = $authenticate;
+    }
+
+    public function __invoke(ServerRequest $request): Identity
+    {
+        if (!$request->headers()->has('Authorization')) {
+            return ($this->authenticate)($request);
+        }
+
+        $header = $request->headers()->get('Authorization');
+
+        if (!$header instanceof Authorization) {
+            throw new MalformedAuthorizationHeader;
+        }
+
+        return ($this->authenticate)($request);
+    }
+}
